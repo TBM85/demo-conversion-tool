8a6b3939169ef918fead1858c05e834def75f3c7
TBM85 <tballesterm@gmail.com>
2021-06-30 23:31:43 -0400

    Include the conversion structure

--- src/components/ToolTabs/ToolTabs.js
+++ src/components/ToolTabs/ToolTabs.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import React from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+
+import equals from "../../assets/icons/equals.svg";
 
 const toolTabs = [
-  {id: 1, title: "Length"},
-  {id: 2, title: "Temperature"},
-  {id: 3, title: "Weight"}
+  {
+    id: 1,
+    title: "Length",
+    units: ["kilometer", "meter", "centimeter", "millimeter", "micrometer", "nanometer", "mile", "yard", "foot", "inch"],
+  },
+  { id: 2, title: "Temperature", units: ["celsius", "fahrenheit"] },
+  { id: 3, title: "Weight", units: ["kilogram", "gram", "milligram", "microgram", "tonne", "stone", "pound", "ounce"] },
 ];
 
 const ToolTabs = () => {
@@ -19,7 +25,35 @@ const ToolTabs = () => {
 
       {toolTabs.map((tab) => (
         <TabPanel key={tab.id}>
-          <h2>{tab.title}</h2>
+          <div className="conversion-container">
+            <div className="conversion-control">
+              <input type="number" className="conversion-input" />
+              <div className="conversion-select">
+                <select
+                  className="form-control"
+                  name="select"
+                >
+                  {tab.units.map((unit) => (
+                    <option key={unit} value={unit}>{unit}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+            <img src={equals} alt="equals" width="20px" height="22px" />
+            <div className="conversion-control">
+              <div className="conversion-output"></div>
+              <div className="conversion-select">
+                <select
+                  className="form-control"
+                  name="select"
+                >
+                  {tab.units.map((unit) => (
+                    <option key={unit} value={unit}>{unit}</option>
+                  ))}
+                </select>
+              </div>
+            </div>
+          </div>
         </TabPanel>
       ))}
     </Tabs>
