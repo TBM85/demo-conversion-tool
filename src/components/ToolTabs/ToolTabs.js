eaf2c81f56704753e63003d00c2325cfcd75d8a2
TBM85 <tballesterm@gmail.com>
2021-07-02 11:52:35 -0400

    Get the selected value in the ToolTabs component

--- src/components/ToolTabs/ToolTabs.js
+++ src/components/ToolTabs/ToolTabs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 
@@ -10,13 +10,61 @@ const toolTabs = [
   {
     id: 1,
     title: "Length",
-    units: ["kilometer", "meter", "centimeter", "millimeter", "micrometer", "nanometer", "mile", "yard", "foot", "inch"],
+    units: [
+      "kilometer",
+      "meter",
+      "centimeter",
+      "millimeter",
+      "micrometer",
+      "nanometer",
+      "mile",
+      "yard",
+      "foot",
+      "inch",
+    ],
   },
   { id: 2, title: "Temperature", units: ["celsius", "fahrenheit"] },
-  { id: 3, title: "Weight", units: ["kilogram", "gram", "milligram", "microgram", "tonne", "stone", "pound", "ounce"] },
+  {
+    id: 3,
+    title: "Weight",
+    units: [
+      "kilogram",
+      "gram",
+      "milligram",
+      "microgram",
+      "tonne",
+      "stone",
+      "pound",
+      "ounce",
+    ],
+  },
 ];
 
 const ToolTabs = () => {
+  const [inputValue, setInputValue] = useState(1);
+  const changeValueHandler = (event) => {
+    setInputValue(event.target.value);
+  };
+
+  const [result, setResult] = useState();
+  useEffect(() => {
+    setResult(inputValue);
+  }, [inputValue]);
+
+  const [inputSelect, setInputSelect] = useState();
+  const inputValueHandler = (selectValue) => {
+    setInputSelect(selectValue);
+  }
+
+  console.log(inputSelect);
+
+  const [outputSelect, setOutputSelect] = useState();
+  const outputValueHandler = (selectValue) => {
+    setOutputSelect(selectValue);
+  }
+
+  console.log(outputSelect);
+
   return (
     <Tabs className={classes.Tabs}>
       <TabList>
@@ -29,13 +77,24 @@ const ToolTabs = () => {
         <TabPanel key={tab.id} className={classes.TabPanel}>
           <div className={classes["conversion-container"]}>
             <div className={classes["conversion-control"]}>
-              <input type="number" className={classes["conversion-input"]} />
-              <Select tab={tab} className={classes["conversion-select"]} />
+              <input
+                type="number"
+                className={classes["conversion-input"]}
+                value={inputValue}
+                onChange={changeValueHandler}
+              />
+              <Select tab={tab} className={classes["conversion-select"]} onSelectValue={inputValueHandler} />
             </div>
-            <img src={swap} alt="equals" width="20px" height="22px" className={classes["conversion-img"]} />
+            <img
+              src={swap}
+              alt="equals"
+              width="20px"
+              height="22px"
+              className={classes["conversion-img"]}
+            />
             <div className={classes["conversion-control"]}>
-              <div className={classes["conversion-output"]}></div>
-              <Select tab={tab} className={classes["conversion-select"]} />
+              <div className={classes["conversion-output"]}>{result}</div>
+              <Select tab={tab} className={classes["conversion-select"]} onSelectValue={outputValueHandler} />
             </div>
           </div>
         </TabPanel>
--- src/components/UI/Select/Select.js
+++ src/components/UI/Select/Select.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import classes from './Select.module.scss';
 
 const Select = (props) => {
+  const [selectValue, setSelectValue] = useState("");
+  const changeSelectHandler = (event) => {
+    let selectedValue = event.target.value;
+
+    setSelectValue(selectedValue);
+  }
+
+  useEffect(() => {
+    props.onSelectValue(selectValue);
+  }, [props, selectValue]);
+
   return (
     <div className={classes["conversion-select"]}>
       <select
         className="form-control"
         name="select"
+        value={selectValue}
+        onChange={changeSelectHandler}
       >
         {props.tab.units.map((unit) => (
           <option key={unit} value={unit}>{unit}</option>
